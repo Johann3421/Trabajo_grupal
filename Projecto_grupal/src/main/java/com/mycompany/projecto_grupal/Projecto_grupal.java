79dce410f8d8bdcdcf255468d4afcc6ee82f0df7
Johann3421 <1355430@senati.pe>
2023-08-17 12:01:44 -0500

    Prueba1

---
+++ Projecto_grupal/src/main/java/com/mycompany/projecto_grupal/Projecto_grupal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecto_grupal;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Projecto_grupal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
